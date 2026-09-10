c7f0c868f6cd396d1777cbde1065094235f5dc0f
3Dever <gamedever96@gmail.com>
2026-09-10 12:18:34 -0400

    Trigger interactables implemented

--- Assets/Scripts/Interactions/Interaction.cs
+++ Assets/Scripts/Interactions/Interaction.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody))]
 public abstract class Interaction : MonoBehaviour
 {
-    public abstract void OnInteract();
+    public UnityEvent InteractEvent;
+
+    public virtual void OnInteract()
+    {
+        InteractEvent?.Invoke();
+    }
 }
